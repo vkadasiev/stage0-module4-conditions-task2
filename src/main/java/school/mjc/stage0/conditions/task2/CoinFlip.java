fa93aa13c7849a111df4f2bd7ded7b76acf9e4d8
Vardan <vkadasiev@gmail.com>
2023-05-24 00:50:27 +0400

    Tasks completed

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        if (from1UpTo1000 < 500) {
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CoinFlip newCoinFlip = new CoinFlip();
+        newCoinFlip.throwCoin(100);
+
 
     }
 }
+
+
+//Implement the program which will consume 1 number in method arguments and
+// if the number is less than 500 print "Eagle"
+// otherwise "Tail" //
+//
+// (range of numbers is [1-1000], no checks for that required):
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first > second) {
+            System.out.println(first);
+        } else if (second > first) {
+            System.out.println(second);
+        } else {
+            System.out.println(first);
+        }
+    }
+
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 10;
+
+        GreatestNumberPrinter printer = new GreatestNumberPrinter();
+        printer.printGreatest(first, second);
+
     }
 }
+
+
+
+//Implement the program that will find the greatest number from two that are passed as parameters to the method and print the greatest.
+// In case of equality print any of them.
